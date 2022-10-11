ab262265d4be8c3fc3c15139311abbc51a3bc5db
Nick <nsagias@gmail.com>
2022-10-11 01:42:09 -0400

    Initialize app

---
+++ Swift Starter App/Swift Starter App/Swift_Starter_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Starter_AppApp.swift
+//  Swift Starter App
+//
+//  Created by nick sagias on 2022-10-11.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Starter_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
