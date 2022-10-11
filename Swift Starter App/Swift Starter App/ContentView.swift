b71795c958e5e77e8d7aa8ceda9fefd7208f10f6
Nick <nsagias@gmail.com>
2022-10-11 02:08:16 -0400

    use HStack

--- Swift Starter App/Swift Starter App/ContentView.swift
+++ Swift Starter App/Swift Starter App/ContentView.swift
@@ -17,6 +17,27 @@ struct ContentView: View {
             Button(action: {}) {
                 Text("Text Button")
             }
+            Circle()
+            HStack {
+                VStack {
+                    // Column 1
+                    Text("1")
+                    Text("1")
+                    Text("1")
+                }
+                VStack {
+                    // Column 2
+                    Text("2")
+                    Text("2")
+                    Text("2")
+                }
+                VStack {
+                    // Column 3
+                    Text("3")
+                    Text("3")
+                    Text("3")
+                }
+            }
         }
         .padding()
       
